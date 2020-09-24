662d99d234ae11a765c5c4a936bb26004b2450ca
Jonathan Narvaez <jonaser06@gmail.com>
2020-09-23 22:29:39 -0500

    fix

--- includes/ld-misc-functions.php
+++ includes/ld-misc-functions.php
@@ -430,7 +430,7 @@ function learndash_payment_buttons( $post ) {
 				$post_button_url = get_home_url( null, $post_button_url );
 			}
 			// $post_button = '<a class="btn-join" href="' . esc_url( $post_button_url ) . '" id="btn-join">' . $button_text . '</a>';
-			$post_button = '<a class="btn-join" href="' . esc_url( $post_button_url ) . '" id="btn-join" style="background: #F25116 !important;">Registrarse</a>';
+			$post_button = '<a class="btn-join" href="' . esc_url( $post_button_url ) . '" id="btn-join" style="background: #84BFB9 !important;">Registrarse</a>';
 		}
 
 		$payment_params = array(
