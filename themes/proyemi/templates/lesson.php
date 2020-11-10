7a9501cff5f6c60979c2e0f9a9bc727c55c11a36
Jonathan Narvaez <jonaser06@gmail.com>
2020-11-09 23:26:30 -0500

    fix

--- themes/proyemi/templates/lesson.php
+++ themes/proyemi/templates/lesson.php
@@ -1,5 +1,29 @@
 <?php
 /* 
+    * duracion tag [ duration h="5"]
+    * resources tag [ resources n="5"]
+    * lesson tag [ lesson n="5"]
+    **/
+    
+    add_shortcode('duration', 'duration');
+    add_shortcode('resources', 'resources');
+    add_shortcode('lesson', 'lesson');
+    add_shortcode('link', 'link2');
+
+    function duration($atts = array()) { return $atts['h']; }
+    function resources($atts = array()) { return $atts['n']; }
+    function lesson($atts = array()) { return $atts['n']; }
+    function link2($atts = array()) { return $atts['l']; }
+
+    preg_match('/\[.*\]/',get_post_field('post_content', $course_id, 'edit'), $match );
+
+    $match = explode(',',$match[0]);
+    $duration = $match[0];
+    $resources = $match[1];
+    $lesson_ = $match[2];
+    $link = $match[3];
+
+/* 
 * duracion tag [ videopro id="5"]
 **/
 
@@ -103,7 +127,7 @@ add_shortcode('comments_wp','get_comentarios');
         <div class="tabs">
             <div class="tab-header">
                 <div class="tab1 active">Modulos</div>
-                <div class="tab2">Contenido</div>
+                <div class="tab2">Recursos</div>
                 <div class="tab3">Comentarios</div>
             </div>
             <div class="tab-content">
@@ -113,7 +137,6 @@ add_shortcode('comments_wp','get_comentarios');
                     <?php $sections = learndash_30_get_course_sections( $course_id ); ?>
                     <?php $i = 0; ?>
                     <?php $all_lesson = count($lessons); ?>
-                    <!-- <?php echo json_encode($lessons); ?> -->
                     <?php foreach($lessons as $lesson) : ?>
                         <?php if ( isset( $sections[ $lesson['post']->ID ] ) ): ?>
                         <div class="heading">
@@ -135,7 +158,14 @@ add_shortcode('comments_wp','get_comentarios');
                     <?php endforeach; ?>
                 </div>
                 <div class="pad2">
-                    <?php echo preg_replace('/\[.*\]/','',get_post_field('post_content', $post->ID, 'edit')); ?>
+                    <div class="resources">
+                        <div class="title-resources">
+                            <a href="<?php echo do_shortcode($link); ?>">
+                            <span><svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 10.7778C13 11.1019 12.8736 11.4128 12.6485 11.642C12.4235 11.8712 12.1183 12 11.8 12H2.2C1.88174 12 1.57652 11.8712 1.35147 11.642C1.12643 11.4128 1 11.1019 1 10.7778V2.22222C1 1.89807 1.12643 1.58719 1.35147 1.35798C1.57652 1.12877 1.88174 1 2.2 1H5.2L6.4 2.83333H11.8C12.1183 2.83333 12.4235 2.9621 12.6485 3.19131C12.8736 3.42052 13 3.7314 13 4.05556V10.7778Z" stroke="#007791" stroke-linecap="round" stroke-linejoin="round"/></svg></span>
+                            Descargar recurso
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div class="pad3">
                     <?php echo do_shortcode( '[comments_wp]' ); ?>
--- themes/proyemi/templates/profile.php
+++ themes/proyemi/templates/profile.php
@@ -30,35 +30,54 @@
 		}
 	}
 	LD_QuizPro::showModalWindow();
+
+	$url = $_SERVER["REQUEST_URI"];
+	$url = explode('/', $url);
+	$url = $url[count($url) - 2];
 ?>
 <div class="squeart-container">
-	<div class="squart-option">
+	<div class="squart-option <?php if($url=='my-account'): ?>active<?php endif; ?>">
 		<a href="<?php echo get_site_url(null, '/my-account/'); ?>">
 			<span><svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M-3.67176e-07 32.2001L14 32.2001M-1.34631e-06 9.8001C2.73367 9.8001 7 9.8001 7 9.8001L14 9.8001M-1.34631e-06 21.0001L7 21.0001M7 21.0001L7 37.8001C7 39.3465 8.2536 40.6001 9.8 40.6001L37.8 40.6001C39.3464 40.6001 40.6 39.3465 40.6 37.8001L40.6 4.2001C40.6 2.6537 39.3464 1.4001 37.8 1.4001L9.8 1.4001C8.2536 1.4001 7 2.65371 7 4.2001L7 21.0001ZM7 21.0001L14 21.0001" stroke="#FEFEFE" stroke-width="2"/></svg></span>
 		</a>
+		<div class="title-opcion">
+			Mis Cursos
+		</div>
 	</div>
-	<div class="squart-option" style="padding-top: 15px;">
+	<div class="squart-option <?php if($url=='orders'): ?>active<?php endif; ?>" style="padding-top: 15px;">
 		<a href="<?php echo get_site_url(null, '/my-account/orders/'); ?>">
 			<span><svg width="44" height="35" viewBox="0 0 44 35" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 0C0.447715 0 0 0.447715 0 1C0 1.55228 0.447715 2 1 2V0ZM42.1244 10.2424L42.9885 10.7457L42.1244 10.2424ZM33.7438 24.631L34.608 25.1343L33.7438 24.631ZM18.7335 24.4115L17.7746 24.6953L18.7335 24.4115ZM10.0678 1.51325L9.19427 2L10.0678 1.51325ZM19.6923 26.1277H32.8797V24.1277H19.6923V26.1277ZM34.608 25.1343L42.9885 10.7457L41.2603 9.73906L32.8797 24.1277L34.608 25.1343ZM9.19427 2L13.2206 9.22581L14.9677 8.25231L10.9414 1.0265L9.19427 2ZM13.1352 9.0229L17.7746 24.6953L19.6923 24.1277L15.053 8.45522L13.1352 9.0229ZM41.2603 7.73906H14.0941V9.73906H41.2603V7.73906ZM9.19427 0H1V2H9.19427V0ZM42.9885 10.7457C43.7651 9.41234 42.8033 7.73906 41.2603 7.73906V9.73906L41.2603 9.73906L42.9885 10.7457ZM32.8797 26.1277C33.5915 26.1277 34.2497 25.7493 34.608 25.1343L32.8797 24.1277L32.8797 24.1277V26.1277ZM19.6923 24.1277L19.6923 24.1277L17.7746 24.6953C18.0261 25.5448 18.8064 26.1277 19.6923 26.1277V24.1277ZM10.9414 1.0265C10.5882 0.392811 9.91969 0 9.19427 0V2L9.19427 2L10.9414 1.0265Z" fill="#FEFEFE"/><ellipse cx="33.1711" cy="31.383" rx="2.68085" ry="2.68085" fill="#FEFEFE"/><ellipse cx="18.8723" cy="31.383" rx="2.68085" ry="2.68085" fill="#FEFEFE"/></svg></span>
 		</a>
+		<div class="title-opcion">
+			Pedidos
+		</div>
 	</div>
-	<div class="squart-option">
+	<div class="squart-option <?php if($url=='downloads'): ?>active<?php endif; ?>">
 		<a href="<?php echo get_site_url(null, '/my-account/downloads/'); ?>">
 			<span><svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.5 31.5L12.75 22.5M22.5 31.5L31.5 22.5M22.5 31.5V3M40.5 21V40.5H4.5V21" stroke="white" stroke-width="1.8"/></svg></span>
 		</a>
+		<div class="title-opcion">
+			Descargas
+		</div>
 	</div>
-	<div class="squart-option">
+	<div class="squart-option <?php if($url=='edit-account'): ?>active<?php endif; ?>">
 		<a href="<?php echo get_site_url(null, '/my-account/edit-account/'); ?>">
 			<span><svg width="33" height="42" viewBox="0 0 33 42" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M25.7547 10.4748C25.7547 15.708 21.599 19.9495 16.4717 19.9495C11.3444 19.9495 7.18868 15.708 7.18868 10.4748C7.18868 5.24153 11.3444 1 16.4717 1C21.599 1 25.7547 5.24153 25.7547 10.4748Z" stroke="white" stroke-width="1.8" stroke-linecap="square"/><path fill-rule="evenodd" clip-rule="evenodd" d="M31.9434 41H1C1 38.756 1 36.6213 1 34.6881C1 29.4519 5.15615 25.2087 10.283 25.2087H22.6604C27.7872 25.2087 31.9434 29.4519 31.9434 34.6881C31.9434 36.6213 31.9434 38.756 31.9434 41Z" stroke="white" stroke-width="1.8" stroke-linecap="square"/></svg></span>
 		</a>
+		<div class="title-opcion">
+			Mi Cuenta
+		</div>
 	</div>
-	<div class="squart-option">
+	<div class="squart-option <?php if($url=='customer-logout'): ?>active<?php endif; ?>">
 		<a href="<?php echo get_site_url(null, '/my-account/customer-logout/'); ?>">
 			<span><svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M27.178 0.49168C32.5936 1.64281 37.3947 4.75029 40.6626 9.21958C43.9305 13.6889 45.4359 19.2062 44.8906 24.7159C44.3454 30.2255 41.7878 35.3407 37.7072 39.0828C33.6267 42.8249 28.3097 44.931 22.7735 44.9983C17.2373 45.0656 11.8707 43.0893 7.70035 39.4476C3.53004 35.8058 0.848885 30.7543 0.169857 25.2595C-0.509171 19.7647 0.861609 14.2124 4.01993 9.66501C7.17825 5.1176 11.9023 1.89433 17.2884 0.611899L17.766 2.61788C12.8736 3.78278 8.58244 6.71065 5.71357 10.8413C2.84471 14.972 1.59955 20.0154 2.21635 25.0066C2.83315 29.9978 5.26858 34.5864 9.0567 37.8944C12.8448 41.2024 17.7196 42.9976 22.7484 42.9364C27.7772 42.8753 32.607 40.9622 36.3135 37.563C40.0201 34.1639 42.3433 29.5175 42.8386 24.5128C43.3339 19.508 41.9665 14.4964 38.9981 10.4367C36.0296 6.377 31.6686 3.55431 26.7493 2.50868L27.178 0.49168Z" fill="white"/><line x1="22" x2="22" y2="28" stroke="white" stroke-width="2"/></svg></span>
 		</a>
+		<div class="title-opcion">
+			Salir
+		</div>
 	</div>
 </div>
-<div id="learndash_profile">
+<div id="learndash_profile" style="overflow: scroll;">
 
     <div class="expand_collapse">
         <a href="#" onClick='return flip_expand_all("#course_list");'><?php esc_html_e( 'Expand All', 'learndash' ); ?></a> | <a href="#" onClick='return flip_collapse_all("#course_list");'><?php esc_html_e( 'Collapse All', 'learndash' ); ?></a>
@@ -114,9 +133,6 @@
 
 	<div id="course_list">
 		<?php 
-			$url = $_SERVER["REQUEST_URI"];
-			$url = explode('/', $url);
-			$url = $url[count($url) - 2];
 			if($url == 'orders' || $url == 'downloads' || $url == 'edit-address' || $url == 'edit-account' | $url == 'customer-logout'):
 				echo do_shortcode('[woocommerce_my_account]');
 			else:
